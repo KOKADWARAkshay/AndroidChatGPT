93febc1b21b02a7573d65518f5b3341b2b0714ec
Akshay Kokadwar <70112790+KOKADWARAkshay@users.noreply.github.com>
2023-04-06 00:35:57 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/akshayandroidgpt/MainActivity.java
+++ app/src/main/java/com/example/akshayandroidgpt/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Bearer key")
                 .post(body)
                 .build();
 
